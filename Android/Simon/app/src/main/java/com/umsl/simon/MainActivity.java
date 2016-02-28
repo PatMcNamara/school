ebba73cc5825fe0df0411cfe73d4a02b0a37745e
coweatyou <pat.mcnam@gmail.com>
2016-02-28 15:21:26 -0600

    Simon finished except rotation

--- Android/Simon/app/src/main/java/com/umsl/simon/MainActivity.java
+++ Android/Simon/app/src/main/java/com/umsl/simon/MainActivity.java
@@ -1,7 +1,5 @@
 package com.umsl.simon;
 
-//TODO you need to have more contrast between the yellows
-
 import android.animation.Animator;
 import android.animation.AnimatorInflater;
 import android.content.SharedPreferences;
@@ -20,10 +18,12 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private MainModel sequence;
-    private Button ulButton, urButton, blButton, brButton;
     private ArrayList<Animator> animator;
+    private ArrayList<Button> buttonList;
     private int position = 0; //TODO this should probably go in the model
     private SharedPreferences highScorePrefs;
+    private int animationSpeed;
+    private final float animationSpeedChange = (float) .8;
     public static final String MODEL_KEY = "com.umsl.simon.model";
     public static final String HIGH_SCORE_PREFS = "HighScore";
 
@@ -34,31 +34,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ulButton = (Button) findViewById(R.id.top_left_button);//TODO this should just be an array list of buttons
-        urButton = (Button) findViewById(R.id.top_right_button);
-        blButton = (Button) findViewById(R.id.bottom_left_button);
-        brButton = (Button) findViewById(R.id.bottom_right_button);
+        buttonList = new ArrayList<Button>();
+        buttonList.add((Button) findViewById(R.id.top_left_button));
+        buttonList.add((Button) findViewById(R.id.top_right_button));
+        buttonList.add((Button) findViewById(R.id.bottom_left_button));
+        buttonList.add((Button) findViewById(R.id.bottom_right_button));
+
+        animationSpeed = 600;
 
         //if((sequence = (MainModel) savedInstanceState.get(MODEL_KEY)) == null) {
             sequence = new MainModel();
         //}
-        animator = new ArrayList<Animator>(); //TODO this should be a map between R.id.* and the animator.
+        animator = new ArrayList<Animator>();
         for(int i = 0; i < 4; i++) {
             Animator a;
 
             if (i == 0) {
                 a = AnimatorInflater.loadAnimator(this, R.animator.green);
-                a.setTarget(ulButton);
             } else if (i == 1) {
                 a = AnimatorInflater.loadAnimator(this, R.animator.red);
-                a.setTarget(urButton);
             } else if (i == 2) {
                 a = AnimatorInflater.loadAnimator(this, R.animator.blue);
-                a.setTarget(blButton);
             } else {
-                a = AnimatorInflater.loadAnimator(this, R.animator.yellow);
-                a.setTarget(brButton);
+                a = AnimatorInflater.loadAnimator(this, R.animator.orange);
             }
+            a.setTarget(buttonList.get(i));
             animator.add(a);
         }
 
@@ -67,8 +67,8 @@ public class MainActivity extends AppCompatActivity {
             highScorePrefs.edit().putInt("HighScore", 0).apply();
         }
 
-        // We can't call displayPopUp until startup is finished so we delay the call.
-        handler.postDelayed(displayStartScreen, 100);
+        // We can't call displayPopUp until the entire view is set up so we delay the call.
+        handler.postDelayed(displayStartScreen, 250);
     }
 
     public void topLeftButtonClicked(View v) {
@@ -84,15 +84,14 @@ public class MainActivity extends AppCompatActivity {
         buttonClicked(3);
     }
 
-    //TODO add start display sequence that will both post delay and disable the buttons
-
     public void buttonClicked(int clicked) {
         animator.get(clicked).start();
         if(sequence.get(position) == clicked) {
             if(++position == sequence.getSize()) { // The user got the entire sequence right
                 sequence.addRandomElementToSequence();
                 position = 0;
-                handler.postDelayed(displayRunnable, 1000);
+                animationSpeed *= animationSpeedChange;
+                startSequenceAnimation();
             }
         } else {
             displayPopUp("Game Over", true);
@@ -103,30 +102,57 @@ public class MainActivity extends AppCompatActivity {
         int currentScore = sequence.getSize() - 1;
         int highScore = highScorePrefs.getInt("HighScore", 0);
 
+        disableColorButtons();
+
         View inflatedView = getLayoutInflater().inflate(R.layout.activity_pop_up, null, false);
         final PopupWindow popup = new PopupWindow(inflatedView, ViewGroup.MarginLayoutParams.WRAP_CONTENT, ViewGroup.MarginLayoutParams.WRAP_CONTENT);
-        popup.showAtLocation(blButton.getRootView(), Gravity.CENTER, 0, 0);
+        popup.showAtLocation(buttonList.get(0).getRootView(), Gravity.CENTER, 0, 0);
         if(highScore < currentScore) {
             highScorePrefs.edit().putInt("HighScore", currentScore).apply();
             highScore = currentScore;
+            title = "New High Score";
         }
 
         ((TextView) inflatedView.findViewById(R.id.popup_title)).setText(title);
+        ((TextView) inflatedView.findViewById(R.id.high_score_text)).setText("High Score: " + highScore);
         if(displayCurrentScore) {
             ((TextView) inflatedView.findViewById(R.id.current_score_text)).setText("Current Score: " + currentScore);
         }
-        ((TextView) inflatedView.findViewById(R.id.high_score_text)).setText("High Score: " + highScore);
 
         ((Button) inflatedView.findViewById(R.id.start_button)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                enableColorButtons();
                 sequence.clear();
                 position = 0;
+                animationSpeed = 600;
                 sequence.addRandomElementToSequence();
-                handler.postDelayed(displayRunnable, 1000);
                 popup.dismiss();
+                startSequenceAnimation();
             }
         });
+    }
+
+    private void disableColorButtons() {
+        for(Button b : buttonList) {
+            b.setEnabled(false);
+        }
+    }
+    private void enableColorButtons() {
+        for(Button b : buttonList) {
+            b.setEnabled(true);
+        }
+    }
+
+    private void startSequenceAnimation() {
+        disableColorButtons();
+        handler.postDelayed(displayRunnable, animationSpeed);
+    }
+
+    private void setTempAnimateDuration(Animator animator, int duration) {
+        long oldDuration = animator.getDuration();
+        animator.setDuration(duration).start();
+        animator.setDuration(oldDuration);
     }
 
     private Runnable displayRunnable = new Runnable() {
@@ -134,10 +160,12 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
             if(animatePosition < sequence.getSize() ) {
-                animator.get(sequence.get(animatePosition++)).start();
-                handler.postDelayed(displayRunnable, 1000); //TODO both of these 2 animators should be time controlled to be longer then get shorter
+                //animator.get(sequence.get(animatePosition++)).setDuration(animationSpeed).start();
+                setTempAnimateDuration(animator.get(sequence.get(animatePosition++)), animationSpeed);
+                handler.postDelayed(displayRunnable, animationSpeed * 2);
             } else { // we are at the end of the sequence
                 animatePosition = 0;
+                enableColorButtons();
             }
         }
     };
